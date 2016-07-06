f10872d16a263645578f641f75f530d8a84fd474
Leo Ahnn <leoahnn@gmail.com>
2016-07-06 18:15:28 -0400

    psuedo code

---
+++ tetris.rb
@@ -0,0 +1,23 @@
+Tetris class
+  def play
+  def lose
+
+Player class
+  def get_move
+
+
+Block class
+  def move_left
+  def move_right
+  def rotate
+  def fall
+
+
+
+Board class
+  def render_grid
+  def make_grid
+  def validiate_move
+  def delete_row
+  def full_row?
+  def make_new_block
